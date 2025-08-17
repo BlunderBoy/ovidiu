93f5082c89e09752958e1059bd0a4305b64ebabf
cristi <cristialex99@gmail.com>
2025-08-18 01:24:37 +0300

    more performance

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,46 +3,63 @@
     import {ArrowRight} from "lucide-svelte";
 
     let imageSrc = 'bg.jpg';
+    //
+    // onMount(() => {
+    //     const section = document.getElementById('hero')!;
+    //     const img = new Image();
+    //     img.src = imageSrc;
+    //     section.onload = () => {
+    //         console.log(section)
+    //         img.onload = () => {
+    //             console.log(section)
+    //             if (section) {
+    //                 console.log('section')
+    //                 section.style.backgroundImage = `url('${img.src}')`;
+    //             }
+    //         };
+    //     }
+    // })
+
 
 </script>
 
 <section
-        class="relative overflow-hidden bg-cover bg-center bg-no-repeat"
+        class="relative overflow-hidden bg-gray-200 bg-cover bg-center bg-no-repeat"
         style="background-image: url({imageSrc}); background-position: center 40%;"
 >
-    <!-- Optional overlay for extra contrast -->
-    <div class="absolute inset-0 bg-black/60"></div>
+<!-- Optional overlay for extra contrast -->
+<div class="absolute inset-0 bg-black/60"></div>
 
-    <!-- Content -->
-    <div class="container relative z-10 grid place-items-center gap-8 mx-auto py-20 md:py-32 text-center mt-20">
-        <div class="space-y-8">
-            <div class="max-w-screen-md mx-auto text-5xl md:text-6xl font-bold text-white">
-                Obține consultanță gratuită
-            </div>
+<!-- Content -->
+<div class="container relative z-10 grid place-items-center gap-8 mx-auto py-20 md:py-32 text-center mt-20">
+    <div class="space-y-8">
+        <div class="max-w-screen-md mx-auto text-5xl md:text-6xl font-bold text-white">
+            Obține consultanță gratuită
+        </div>
 
-            <p class="max-w-screen-sm mx-auto text-2xl text-white">
-                Suntem aici să te ajutăm. Lasă datele tale de contact în formularul alăturat și un coleg te va contacta
-                pentru asistență.
-            </p>
+        <p class="max-w-screen-sm mx-auto text-2xl text-white">
+            Suntem aici să te ajutăm. Lasă datele tale de contact în formularul alăturat și un coleg te va contacta
+            pentru asistență.
+        </p>
 
-            <div class="space-y-4 md:space-y-0 md:space-x-4">
-                <a href="#contact">
-                    <Button class="w-5/6 md:w-1/4 font-bold group/arrow mb-4">
-                        Contacteaza-ne!
-                        <ArrowRight
-                                class="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform"
-                        />
-                    </Button>
-                </a>
+        <div class="space-y-4 md:space-y-0 md:space-x-4">
+            <a href="#contact">
+                <Button class="w-5/6 md:w-1/4 font-bold group/arrow mb-4">
+                    Contacteaza-ne!
+                    <ArrowRight
+                            class="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform"
+                    />
+                </Button>
+            </a>
 
-                <a href="#despre">
-                    <Button class="w-5/6 md:w-1/4 font-bold" variant="secondary">
-                        Afla mai multe!
-                    </Button>
-                </a>
-            </div>
+            <a href="#despre">
+                <Button class="w-5/6 md:w-1/4 font-bold" variant="secondary">
+                    Afla mai multe!
+                </Button>
+            </a>
         </div>
     </div>
+</div>
 </section>
 
 
